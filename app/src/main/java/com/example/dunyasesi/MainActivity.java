4713e9d7f3ac2ae8161598f3ce9f8b7539873aa2
Mohamed Jafari Mnete <mnetemohamed@gmail.com>
2021-02-26 06:13:16 -0600

    Update MainActivity.java
    Just an example of a commit.

--- app/src/main/java/com/example/dunyasesi/MainActivity.java
+++ app/src/main/java/com/example/dunyasesi/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    
+    int number = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
